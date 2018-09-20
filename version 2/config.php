7e322733055ae6f557fc76a25db4715a5e96ac0b
Riyad Hisham <nfzrz36@gmail.com>
2018-09-20 17:04:21 +0800

    version 2

---
+++ version 2/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'kedaiduludulu';
+$databaseUsername = 'root';
+$databasePassword = '123456';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName );
+?>
